3425647f57948b7d6660e3abb5c2806c30deee72
Gintaras Bukontas <gintaras.bukontas@gmail.com>
2024-02-26 20:18:59 +0200

    start of JS

---
+++ JavaScript/Paskaita 1 2024-02-26/Praktika/script.js
@@ -0,0 +1,15 @@
+// First task
+
+let name;
+let admin;
+
+name = "Jonas";
+
+admin = name;
+
+alert(admin);
+
+// Second task
+
+const ourPlanet = "Earth";
+let username = "Gintaras";
---
+++ JavaScript/Paskaita 1 2024-02-26/js/script.js
@@ -0,0 +1,36 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+let message = 50;
+let name = "Vardas";
+
+let someString = 'cia eina kazkoks sakinys 2330čą0ę@#)@';
+
+message = name;
+
+// alert(message);
+
+// message = 100;
+
+// alert(message);
+
+// Numbers
+const birthYear = 1994;
+const number1 = -55;
+const number2 = 2.6456;
+
+// Strings
+const string1 = "abc";
+const string2 = 'čęčėč@@$$%'
+const string3 = "22323";
+const string4 = `Hello`;
+
+// Booleans
+const bool1 = true;
+const bool2 = false;
+
+// Null
+const null1 = null;
+
+// Undefined
+const undefined1 = undefined;
